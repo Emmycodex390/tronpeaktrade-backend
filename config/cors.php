93ac74dd042c44734178af6727389411b3ffaf72
Emmycodex390 <emmanuelobi50006@gmail.com>
2026-08-05 06:12:58 +0000

    Fix CORS allowed origins for production domain

--- config/cors.php
+++ config/cors.php
@@ -30,7 +30,7 @@ return [
         env('FRONTEND_URL', env('APP_URL')),
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://cefixtrades.org',
+        'https://fexistrades.org',
         'http://localhost:8000',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
